82f77d16e506df176cceb75a50387ee8a97d1f2f
skippy12345 <jaredmurray012@gmail.com>
2016-10-12 12:56:50 -0500

    Revert "Revert "Revamped the project""
    This reverts commit dfcca6bf7b90a031022b8cdd194e95035a553de5.

---
+++ SE_Group_Awesome1/src/application/ControllerMenu.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ControllerMenu implements Initializable
+{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) 
+	{
+		
+		
+	}
+	
+
+}
