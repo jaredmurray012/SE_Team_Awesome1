acc0bc207dd77cc6d3316f557df9140ff50c4174
skippy12345 <jaredmurray012@gmail.com>
2016-11-02 10:51:47 -0500

    Refactored Some Bad Commits
    Certain people uploaded a folder with files, as opposed to just the
    individual files. So if everything is correct this should fix the
    errors.

---
+++ SE_Group_Awesome1/src/application/ControllerBreakfast.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+public class ControllerBreakfast implements Initializable
+{
+	
+	@FXML 
+	public Button idHotOffTheGriddle;
+	@FXML 
+	public Button idHealthyOptions;
+	
+	@FXML 
+	public Button idOmelets;
+
+	@FXML 
+	public Button idSkillets;
+	
+	@FXML 
+	public Button idFamousBenedicts;
+	
+	@FXML 
+	public Button idCountryFavorites; 
+	
+	@FXML
+	public Button idBreakfastSandwiches;
+	@FXML
+	public Button idBreakfastMasterpieces;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
---
+++ SE_Group_Awesome1/src/application/ControllerBreakfastSandwiches.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+public class ControllerBreakfastSandwiches implements Initializable
+{
+	
+	@FXML 
+	public Button idCheese;
+	@FXML 
+	public Button idSausageHamBacon;
+	
+	@FXML 
+	public Button idVeggie;
+
+	@FXML 
+	public Button idFriedEggSand;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
