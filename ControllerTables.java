a4ab7e8dbaea36a9ccddd8728577c4c535c8b8a8
xcortez01 <xcortez01@aurora.edu>
2016-11-25 08:50:01 -0600

    FXML for tables
    this is what i have so far im currently working on the dropdown menu

---
+++ ControllerTables.java
@@ -0,0 +1,68 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class ControllerTables {
+
+	@FXML 
+	public Button Table1;
+	@FXML 
+	public Button Table2;
+	
+	@FXML 
+	public Button Table3;
+
+	@FXML 
+	public Button Table4;
+	
+	@FXML 
+	public Button Table5;
+	
+	@FXML 
+	public Button Table6; 
+	
+	@FXML
+	public Button Table7;
+	@FXML
+	public Button Table8;
+	
+	@FXML 
+	public Button Table9;
+	
+	@FXML 
+	public Button Table10;
+	
+	@FXML 
+	public Button Table11;
+
+	@FXML 
+	public Button Table12;
+	
+	@FXML 
+	public Button Table13;
+	
+	@FXML 
+	public Button Table14; 
+	
+
+public class ControllerMenu implements Initializable
+{
+	@FXML 
+	public Button Back;
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) 
+	{
+		
+		
+	}
+	
+}
+
